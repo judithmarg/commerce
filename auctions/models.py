57bbae165f2752adc98e8e1540d18f266b99657f
judithMarg <judithmargaritapacollalli@gmail.com>
2023-08-26 01:14:43 -0400

    add class category

--- auctions/models.py
+++ auctions/models.py
@@ -5,12 +5,19 @@ from django.db import models
 class User(AbstractUser):
     pass
 
+class Category(models.Model):
+    name_category = models.CharField(max_length=25)
+
+    def __str__(self):
+        return f'{self.id} {self.name_category}'
+
+
 class AuctionListing(models.Model):
     title = models.CharField(max_length=25)
     description = models.CharField(max_length=64)
     start_bid = models.FloatField()
     image = models.ImageField() ##url
-    category = models.CharField(max_lenght=25)
+    list_category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='categories')
 
     def __str__(self):
         return f'{self.id}: {self.title} , {self.description} with {self.start_bid}'
